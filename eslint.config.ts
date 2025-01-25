952b20be496cb4f2ac105d388d93f83e00c78b6c
Thomas <thomas.loy@posteo.de>
2025-01-25 18:23:43 +0100

    fix(linter): update linter rules

---
+++ eslint.config.ts
@@ -0,0 +1,129 @@
+// @ts-check
+import vueParser from 'vue-eslint-parser'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import vuePlugin from 'eslint-plugin-vue'
+import type { Linter } from 'eslint'
+import tseslint from 'typescript-eslint'
+import globals from 'globals'
+import url from 'node:url'
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
+
+// const config: Linter.Config[] = [
+//   {
+//     ignores: ['node_modules', 'dist/**/*', 'out/**/*'], // Add any ignored patterns here
+//     files: ['**/*.{js,ts,vue}'], // Specify the file types this configuration applies to
+//     languageOptions: {
+//       parser: vueParser, // Use Vue ESLint parser
+//       parserOptions: {
+//         parser: '@typescript-eslint/parser', // Use TypeScript parser within Vue
+//         ecmaVersion: 'latest', // Enable modern ECMAScript features
+//         sourceType: 'module' // Allow ES modules
+//       },
+//       globals: {
+//         ...globals.browser,
+//         ...globals.node
+//       }
+//     } as Linter.LanguageOptions,
+//     plugins: {
+//       ['@typescript-eslint']: tseslint.plugin,
+//       vue: vuePlugin
+//     },
+//     linterOptions: {
+//       reportUnusedDisableDirectives: 'off'
+//     },
+
+//     // ...tseslint.configs.recommendedTypeChecked,
+//     rules: {
+//       // Include the rules from vue3-recommended directly
+//       ...typescriptEslint.configs['eslint-recommended'].rules,
+//       ...vuePlugin.configs['vue3-recommended'].rules,
+//       // Custom rules
+//       '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+//       '@typescript-eslint/explicit-function-return-type': 'warn',
+//       '@typescript-eslint/explicit-module-boundary-types': 'off',
+//       '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+//       '@typescript-eslint/no-explicit-any': 'error',
+//       '@typescript-eslint/no-non-null-assertion': 'off',
+//       '@typescript-eslint/no-var-requires': 'off',
+//       'vue/multi-word-component-names': 'off' // Example of a Vue-specific rule
+//     } as Linter.RulesRecord
+//   },
+//   {
+//     files: ['*.js'], // Override rules specifically for JavaScript files
+//     rules: {
+//       '@typescript-eslint/explicit-function-return-type': 'off'
+//     }
+//   },
+//   ...(tseslint.configs.recommendedTypeChecked as Linter.Config[]),
+//   {
+//     languageOptions: {
+//       parserOptions: {
+//         projectService: {
+//           allowDefaultProject: ['*.ts']
+//         },
+//         tsconfigRootDir: import.meta.dirname,
+//         allowDefaultProject: true
+//       }
+//     }
+//   }
+// ]
+
+// export default config
+
+export default tseslint.config(
+  // register all of the plugins up-front
+  {
+    // note - intentionally uses computed syntax to make it easy to sort the keys
+    plugins: {
+      ['@typescript-eslint']: tseslint.plugin,
+      vue: vuePlugin,
+      ['@typescript-eslint/eslint-plugin']: typescriptEslint
+    },
+    settings: {
+      perfectionist: {
+        order: 'asc',
+        partitionByComment: true,
+        type: 'natural'
+      }
+    }
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', 'out/**/*', '**/*.mjs', '**/*.js'], // Add any ignored patterns here
+  },
+  vuePlugin.configs['flat/recommended'],
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  // base config
+  {
+    languageOptions: {
+      globals: {
+        ...globals.es2020,
+        ...globals.node,
+        ...globals.browser
+      },
+      parser: vueParser, // Use Vue ESLint parser
+      parserOptions: {
+        parser: '@typescript-eslint/parser', // Use TypeScript parser within Vue
+        projectService: true,
+        tsconfigRootDir: __dirname,
+        allowDefaultProject: true,
+        warnOnUnsupportedTypeScriptVersion: false,
+        extraFileExtensions: ['.vue']
+      }
+    },
+    linterOptions: { reportUnusedDisableDirectives: 'off' },
+    rules: {
+      // Include the rules from vue3-recommended directly
+      ...vuePlugin.configs['vue3-recommended'].rules,
+      // Custom rules
+      '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/no-var-requires': 'off'
+    } as Linter.RulesRecord
+  }
+)
