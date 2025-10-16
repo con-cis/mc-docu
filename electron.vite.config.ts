f9564c15a9f0c7bffe04153cc0839a605fd8a50e
con-cis <54837156+con-cis@users.noreply.github.com>
2025-10-16 22:35:07 +0200

    Update electron.vite.config.ts

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,24 @@
-import { resolve } from 'path'
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import vue from '@vitejs/plugin-vue'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin({})]
+    plugins: [externalizeDepsPlugin()],
   },
   preload: {
-    plugins: [externalizeDepsPlugin({})]
+    plugins: [externalizeDepsPlugin()],
   },
   renderer: {
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        '@renderer': resolve(__dirname, 'src/renderer/src'),
+      },
     },
-    plugins: [vue()]
-  }
+    plugins: [vue()],
+  },
 })
